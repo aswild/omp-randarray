f39818ec9b29950d72ed3470a608f7aa38b359e5
Allen Wild <allenwild93@gmail.com>
2017-10-08 02:22:34 -0400

    Random array generator with OpenMP

--- randarray.c
+++ randarray.c
@@ -4,8 +4,30 @@
 #include <unistd.h>
 #include "timer.h"
 
+#if WITH_OPENMP
+#include <omp.h>
+#else
+// stubs for compiling without omp
+#define omp_get_thread_num() 0
+#define omp_set_num_threads(...)
+#endif
+
 #define RD_STATE_LEN 128
 #define CHECKPOINT fprintf(stderr, "%s:%d CHECKPOINT\n", __func__, __LINE__);
+
+#define MALLOC(var, size) do { \
+    var = malloc(size); \
+    if (var == NULL) { \
+        fprintf(stderr, "%s: malloc of %zu bytes failed for " #var, __func__, size); \
+        exit(1); \
+    }} while(0)
+
+#define CALLOC(var, size1, size2) do { \
+    var = calloc(size1, size2); \
+    if (var == NULL) { \
+        fprintf(stderr, "%s: calloc of %lu bytes failed for " #var, __func__, (long unsigned int)(size1 * size2)); \
+        exit(1); \
+    }} while(0)
 
 void usage(void)
 {
@@ -16,21 +38,21 @@ void usage(void)
         "  -n SIZE      The size of the square array\n"
         "  -N LOGSIZE   The log2 of the size of the square array\n"
         "               (LOGSIZE=10 means 1024x1024)\n"
+        "  -T THREADS   number of threads\n"
         ;
     puts(usage_text);
 }
 
 int main(int argc, char *argv[])
 {
-    atimer_t timer;
-    unsigned int seed;
-    struct random_data *rd;
-    char *rd_state;
+    unsigned int *seeds;
     int opt;
     size_t arr_size = 1024;
     int32_t *arr;
+    int nthreads = 1;
+    atimer_t timer;
 
-    while ((opt = getopt(argc, argv, "hn:N:")) != -1)
+    while ((opt = getopt(argc, argv, "hn:N:T:")) != -1)
     {
         switch (opt)
         {
@@ -50,6 +72,14 @@ int main(int argc, char *argv[])
                 }
                 break;
 
+            case 'T':
+#if WITH_OPENMP
+                nthreads = strtol(optarg, NULL, 0);
+#else
+                fprintf(stderr, "compiled without OMP, single-thread only\n");
+#endif
+                break;
+
             default:
                 usage();
                 exit(1);
@@ -57,35 +87,54 @@ int main(int argc, char *argv[])
         }
     }
 
+    printf("Using %d threads\n", nthreads);
     printf("Using array size %zu (%zu bytes)\n", arr_size, arr_size * sizeof(*arr));
-    arr = malloc(arr_size * sizeof(*arr));
-    if (!arr)
-    {
-        printf("malloc failed!\n");
-        exit(1);
-    }
+    MALLOC(arr, arr_size * sizeof(*arr));
 
-    timer_init(&timer);
+    // init seeds
+    MALLOC(seeds, nthreads * sizeof(*seeds));
     srandom(time(NULL));
-    seed = random();
-    rd = calloc(1, sizeof(*rd));
-    rd_state = calloc(1, RD_STATE_LEN);
-    initstate_r(seed, rd_state, RD_STATE_LEN, rd);
-    srandom_r(seed, rd);
+    for (int i = 0; i < nthreads; i++)
+        seeds[i] = random();
 
+    omp_set_num_threads(nthreads);
+
+    // init and start timer, immediately before parallel region
+    timer_init(&timer);
     timer_start(&timer);
-    for (size_t i = 0; i < arr_size; i++)
+
+/* start threads */
+#pragma omp parallel
     {
-        random_r(rd, &arr[i]);
+        // everything defined in this block is thread-private
+        // everything defined outside of the parallel region is shared
+        struct random_data *rd;
+        char *rd_state;
+        int tnum = omp_get_thread_num();
+
+        // init prng
+        CALLOC(rd, 1, sizeof(*rd));
+        CALLOC(rd_state, 1, RD_STATE_LEN);
+        initstate_r(seeds[tnum], rd_state, RD_STATE_LEN, rd);
+        srandom_r(seeds[tnum], rd);
+
+/* magic happens here */
+#pragma omp for
+        for (size_t i = 0; i < arr_size; i++)
+            random_r(rd, &arr[i]);
+
+        free(rd);
+        free(rd_state);
     }
+/* end parallel region */
+
     timer_stop(&timer);
+    free(seeds);
 
     printf("finished in ");
     timer_print(&timer, stdout);
     printf("\n");
 
-    free(rd);
-    free(rd_state);
     free(arr);
     return 0;
 }
